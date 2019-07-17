507ab8cd248b03359899b39ee3ce07db532af90b
Kevin Patel <kpatel1209@gmail.com>
2019-07-16 22:59:09 -0400

    minor adjustment to routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/auth
-// @desc    Test route
-// @access  Public
+const User = require('../../models/User');
+
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
@@ -20,51 +21,38 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
-// @route   POST api/auth
-// @desc    Authenticate user & get token
-// @access  Public
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
 router.post(
   '/',
   [
-    check('email', 'Please enter a valid email address'),
-    check('password', 'Please is required').exists()
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
 
     try {
-      let user = await User.findOne({
-        email
-      });
+      let user = await User.findOne({ email });
 
-      // See if the user exists
       if (!user) {
-        return res.status(400).json({
-          errors: [
-            {
-              msg: 'Invalid Credentials'
-            }
-          ]
-        });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({
-          errors: [
-            {
-              msg: 'Invalid Credentials'
-            }
-          ]
-        });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
 
       const payload = {
@@ -76,14 +64,10 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        {
-          expiresIn: 360000
-        },
+        { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          res.json({
-            token
-          });
+          res.json({ token });
         }
       );
     } catch (err) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,50 +8,38 @@ const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   '/',
   [
     check('name', 'Name is required')
       .not()
       .isEmpty(),
-    check('email', 'Please enter a valid email address'),
+    check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
-    ).isLength({
-      min: 6
-    })
+    ).isLength({ min: 6 })
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
     try {
-      let user = await User.findOne({
-        email
-      });
+      let user = await User.findOne({ email });
 
-      // See if the user exists
       if (user) {
-        res.status(400).json({
-          errors: [
-            {
-              msg: 'User already exists'
-            }
-          ]
-        });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      // Get users' gravatar
       const avatar = gravatar.url(email, {
         s: '200',
         r: 'pg',
@@ -65,7 +53,6 @@ router.post(
         password
       });
 
-      // Encrypt password with brcypt
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
